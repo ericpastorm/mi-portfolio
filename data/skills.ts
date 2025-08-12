ade23b3ab972b7f5ab750d8a61a48ba522f6fbd2
eric <38719351+ericpastorm@users.noreply.github.com>
2025-08-12 19:55:16 +0200

    Update skills data

--- data/skills.ts
+++ data/skills.ts
@@ -2,8 +2,8 @@
 
 import {
   SiJavascript, SiTypescript, SiReact, SiHtml5, SiCss3, SiSass, SiTailwindcss,
-  SiNodedotjs, SiExpress, SiNextdotjs, SiPython, SiMongodb, SiFirebase,
-  SiGit, SiGithub, SiPostman, SiVercel, SiFigma
+  SiNodedotjs, SiNextdotjs, SiPython, SiMongodb, SiFirebase,
+  SiGit, SiGithub, SiVercel, SiFigma, SiBootstrap, SiDocker, SiVite, SiDjango, SiPostgresql, SiSupabase, SiAdobeillustrator
 } from 'react-icons/si';
 
 export type Skill = {
@@ -23,26 +23,31 @@ export const skillsData: SkillCategory[] = [
     skills: [
       { name: "HTML5", icon: SiHtml5, color: "#E34F26" },
       { name: "CSS3", icon: SiCss3, color: "#1572B6" },
-      { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E" },
-      { name: "React", icon: SiReact, color: "#61DAFB" },
-      { name: "Tailwind CSS", icon: SiTailwindcss, color: "#38BDF8" },
       { name: "Sass", icon: SiSass, color: "#CC6699" },
+      { name: "Bootstrap", icon: SiBootstrap, color: "#563D7C" },
+      { name: "Tailwind CSS", icon: SiTailwindcss, color: "#38BDF8" },
+      { name: "React", icon: SiReact, color: "#61DAFB" },
     ],
   },
   {
     title: "Backend",
     skills: [
+      { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E" },
+      { name: "TypeScript", icon: SiTypescript, color: "#3a79c1" },
       { name: "Node.js", icon: SiNodedotjs, color: "#339933" },
-      { name: "Express", icon: SiExpress, color: "#000000" }, 
       { name: "Next.js", icon: SiNextdotjs, color: "#000000" },
+      { name: "Vite", icon: SiVite, color: "#fcd14f" },
       { name: "Python", icon: SiPython, color: "#3776AB" },
+      { name: "Django", icon: SiDjango, color: "#224e44" },
     ],
   },
   {
     title: "Bases de Datos",
     skills: [
+        { name: "Postgresql", icon: SiPostgresql, color: "#37618f" },
         { name: "MongoDB", icon: SiMongodb, color: "#47A248" },
         { name: "Firebase", icon: SiFirebase, color: "#FFCA28" },
+        { name: "Supabase", icon: SiSupabase, color: "#61ca93" },
     ],
   },
   {
@@ -50,9 +55,10 @@ export const skillsData: SkillCategory[] = [
     skills: [
       { name: "Git", icon: SiGit, color: "#F05032" },
       { name: "GitHub", icon: SiGithub, color: "#181717" },
-      { name: "Postman", icon: SiPostman, color: "#FF6C37" },
       { name: "Vercel", icon: SiVercel, color: "#000000" },
+      { name: "Docker", icon: SiDocker, color: "#2496ED" },
       { name: "Figma", icon: SiFigma, color: "#F24E1E" },
+      { name: "Illustrator", icon: SiAdobeillustrator, color: "#f79f35" },
     ],
   },
 ];
